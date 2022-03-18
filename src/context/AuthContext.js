ece0db8af1291980f0560b3196d57ee4826998aa
ryan-school2020 <ryan.khursheedps@gmail.com>
2022-03-18 06:25:16 +0530

    firebase Setup & AuthProvider

---
+++ src/context/AuthContext.js
@@ -0,0 +1,42 @@
+import { createContext, useEffect, useReducer } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase/config";
+
+export const AuthContext = createContext();
+
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case "AUTH_IS_READY":
+      return { ...state, user: action.payload, authIsReady: true };
+
+    case "LOGIN":
+      return { ...state, user: action.payload };
+
+    case "LOGOUT":
+      return { ...state, user: null };
+
+    default:
+      return { ...state };
+  }
+};
+
+export function AuthProvider({ children }) {
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+    authIsReady: false,
+  });
+
+  useEffect(() => {
+    const checkAuthOnce = onAuthStateChanged(auth, (user) => {
+      dispatch({ type: "AUTH_IS_READY", payload: user });
+    });
+
+    checkAuthOnce();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
