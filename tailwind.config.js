015997dbb2506568f1eee89af927978ed57fa999
ryan-school2020 <ryan.khursheedps@gmail.com>
2022-03-23 04:48:35 +0530

    update toast bg & add scrollbar hide

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
@@ -7,5 +9,38 @@ module.exports = {
     extend: {},
   },
   darkMode: "class",
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities(
+        {
+          ".scrollbar-hide": {
+            /* IE and Edge */
+            "-ms-overflow-style": "none",
+
+            /* Firefox */
+            "scrollbar-width": "none",
+
+            /* Safari and Chrome */
+            "&::-webkit-scrollbar": {
+              display: "none",
+            },
+          },
+
+          ".scrollbar-default": {
+            /* IE and Edge */
+            "-ms-overflow-style": "auto",
+
+            /* Firefox */
+            "scrollbar-width": "auto",
+
+            /* Safari and Chrome */
+            "&::-webkit-scrollbar": {
+              display: "block",
+            },
+          },
+        },
+        ["responsive"]
+      );
+    }),
+  ],
 };
