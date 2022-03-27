201c3e5ba5460e460bbf2d9aeeedab0d021d5764
ryan-school2020 <ryan.khursheedps@gmail.com>
2022-03-27 15:30:16 +0530

    update useToast & add useWindowDimensions hook

--- src/hooks/useToast.js
+++ src/hooks/useToast.js
@@ -1,28 +1,37 @@
+import { useMemo, useCallback, useState } from "react";
 import { toast } from "react-toastify";
 import useTheme from "./useTheme";
 
 const useToast = (duration) => {
+  const [, render] = useState(null);
   const { themeMode } = useTheme();
 
-  const toastConfig = {
-    position: "top-right",
-    autoClose: duration,
-    theme: themeMode,
-  };
+  const toastConfig = useMemo(
+    () => ({
+      position: "top-right",
+      autoClose: duration,
+      theme: themeMode,
+    }),
+    [themeMode, duration]
+  );
 
-  const createToast = (message, type) => {
-    if (type === "error") {
-      toast.error(message, toastConfig);
-    }
+  const createToast = useCallback(
+    (message, type) => {
+      render();
+      if (type === "error") {
+        toast.error(message, toastConfig);
+      }
 
-    if (type === "success") {
-      toast.success(message, toastConfig);
-    }
+      if (type === "success") {
+        toast.success(message, toastConfig);
+      }
 
-    if (!type) {
-      toast.info(message, toastConfig);
-    }
-  };
+      if (!type) {
+        toast.info(message, toastConfig);
+      }
+    },
+    [toastConfig]
+  );
 
   return { createToast };
 };
